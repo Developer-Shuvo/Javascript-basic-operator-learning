9835b116a3543966f98928cc34c50061eb7da015
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-03-09 21:05:44 +0600

    Basic of js using

---
+++ Variable.js
@@ -0,0 +1,80 @@
+// // String type
+// var address = "Tejgaon, Dhaka";
+// console.log(address);
+
+
+// // Number type
+// var age = 25;
+// console.log(age);
+
+// // Boolean type
+// var isvillain = true;
+// console.log(isvillain);
+
+// var amiPanKori = true;
+// console.log(amiPanKori);
+
+
+
+// // Practice session
+// for (let i = 0; i < 1000; i++) {
+//     console.log(" i love you jakia ");
+
+// }
+
+// for (let a = 10; a <= 100; a++) {
+//     console.log("Hello World");
+// }
+
+// for (let a = 10; a < 5000; a++) {
+//     console.log("I don't Know him, Please trust me, i can say this 5000 times anymore for you. atleast trust me baby")
+// }
+
+
+// var applePrice = 10;
+// var numberOfApple = 5;
+// var totalPrice = applePrice * numberOfApple;
+// console.log(totalPrice);
+
+// var mangoPrice = 20;
+// var bananaPrice = parseInt("5");
+
+// console.log(mangoPrice + bananaPrice);
+
+
+// var applePrice = 10;
+// var lemonPrice = parseFloat("20.5");
+// totalPrice = applePrice + lemonPrice;
+// console.log(totalPrice);
+
+// var myCash = 1000;
+// var applePrice = 330;
+// var grapePrice = 390;
+// var datePrice = 240;
+// var totalPrice = applePrice + grapePrice + datePrice;
+// var returnCash = myCash - totalPrice;
+// console.log(returnCash);
+
+
+// var math = 80;
+// var physics = 70;
+// var chemistry = 90;
+// var biology = 85;
+// var english = 75;
+// var totalMarks = math + physics + chemistry + biology + english;
+// var average = (totalMarks / 5);
+// console.log(average);
+
+
+// var alu = 30;
+// var begun = 80;
+// var borboti = 160;
+// var peyaj = 40;
+// var totalPrice = alu + begun + borboti + peyaj;
+// var averagePrice = totalPrice / 4;
+// console.log(averagePrice);
+
+
+var cash = 2735;
+var vagsesh = cash % 3;
+console.log(vagsesh);
