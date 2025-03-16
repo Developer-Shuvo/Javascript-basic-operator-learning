0d0bf7347fffac082438538120d07f8e8f8bd714
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-03-16 12:14:13 +0600

    For Loop & While loop basic

--- Loop/for-of.js
+++ Loop/for-of.js
@@ -1,6 +1,45 @@
 // *************Using num********************
-const Students = [12, 34, 56, 78, 90];
+// const Students = [12, 34, 56, 78, 90];
 
-for (const num of Students) {
+// for (const num of Students) {
+//     console.log(num);
+// }
+
+
+
+
+// ***********Using Num in string*******************
+// const shopping = ['Milk', 'Bread', 'Egg', 'Butter', 'Cheese'];
+
+// for (const num of shopping) {
+//     console.log(num);
+// }
+
+
+// ***********For loop*****************
+
+// const bike = ["RoyalEnfield", "R15 V3", "Suzuki", "Pulsar"];
+
+// for (const num of bike) {
+//     console.log(num);
+// }
+
+
+// ***************While Loop********************
+
+// let number = 50;
+
+// while (number <= 100) {
+//     console.log(number);
+//     number++;
+// }
+
+
+
+// ****************For loop*******************
+
+let shoppingItems = ['Milk', 'Bread', 'Egg', 'Butter', 'Cheese'];
+
+for (const num of shoppingItems) {
     console.log(num);
 }
