97331e4a3093f8cff2c591a3551e4ee231407cfb
SHUVO <im.computer.engineeer@gamil.com>
2025-05-10 21:36:36 +0600

    Object into object

---
+++ More-about ES6/chain.js
@@ -0,0 +1,13 @@
+// Learning about objects in JavaScript
+const information = [{Name: 'Shuvo', Age: 22, HomeTown: 'Tangail', profession: 'Student'}];
+// console.log('=', information[0].Age);
+
+
+
+const shuvo = [{Name: 'Md. Nazmul Hasan Shuvo',
+     Dream: 'Professional Web Developer',
+     Education: [{Bsc: 'BSc in CSE', University: 'Daffodil International University', 
+        Diploma: 'Diploma in CST', College: 'Mymensingh Polytechnic Institute'}]
+}];
+
+console.log (shuvo[0].Education[0].Diploma);
