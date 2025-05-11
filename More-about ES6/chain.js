62945237ec00940e3ddac3a1a4d60f3cc0b7e458
SHUVO <im.computer.engineeer@gamil.com>
2025-05-11 12:25:28 +0600

    maping

--- More-about ES6/chain.js
+++ More-about ES6/chain.js
@@ -1,4 +1,7 @@
 // Learning about objects in JavaScript
+// objects into another object
+
+
 const information = [{Name: 'Shuvo', Age: 22, HomeTown: 'Tangail', profession: 'Student'}];
 // console.log('=', information[0].Age);
 
@@ -10,4 +13,19 @@ const shuvo = [{Name: 'Md. Nazmul Hasan Shuvo',
         Diploma: 'Diploma in CST', College: 'Mymensingh Polytechnic Institute'}]
 }];
 
-console.log (shuvo[0].Education[0].Diploma);
+// console.log (shuvo[0].Education[0].Diploma);
+
+
+
+const OfficialInformation = {
+        name: 'Kamal Uddin',
+        age: 30,
+        extraKnowledge: {
+                expertOn: 'Web Development',
+                experience: '2 years',
+                skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'],
+        }
+}
+
+
+console.log(OfficialInformation.extraKnowledge.skills[1]);
---
+++ More-about ES6/map.js
@@ -0,0 +1,19 @@
+// Learning to Map
+
+const money = [100, 200, 300, 400, 500];
+const doubleMoney = money.map (num => num * 2);
+// console.log (doubleMoney);
+
+
+
+// double
+const numbers = [1, 2, 3, 4, 5];
+const doubleNumbers = numbers.map (num => num*2);
+console.log (doubleNumbers);
+
+
+// multiply 
+
+const numbers2 = [1, 2, 3, 4, 5];
+const multiplyNumbers = numbers2.map(num => num * 5);
+console.log (multiplyNumbers);
