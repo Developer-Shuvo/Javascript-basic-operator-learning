7ec92a9f39312ff63800ec3b9083bbee2e6e8cbc
SHUVO <im.computer.engineeer@gamil.com>
2025-05-11 13:46:46 +0600

    For Each practice

---
+++ More-about ES6/for-each.js
@@ -0,0 +1,12 @@
+// For Each loop practice
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.forEach (num => console.log(num * 2));
+
+
+// ------------------------------
+
+const money = [100, 200, 300, 400, 500];
+const calculateMoney = money.forEach (amount => console.log (amount * 5));
+
+
+
--- More-about ES6/map.js
+++ More-about ES6/map.js
@@ -9,11 +9,18 @@ const doubleMoney = money.map (num => num * 2);
 // double
 const numbers = [1, 2, 3, 4, 5];
 const doubleNumbers = numbers.map (num => num*2);
-console.log (doubleNumbers);
+// console.log (doubleNumbers);
 
 
 // multiply 
 
 const numbers2 = [1, 2, 3, 4, 5];
 const multiplyNumbers = numbers2.map(num => num * 5);
-console.log (multiplyNumbers);
+// console.log (multiplyNumbers);
+
+
+
+// Friends
+const friends = ['Shuvo', 'Hamim', 'Urman', 'Vikram', 'Opu'];
+const friendsLength = friends.map (friend => friend.length);
+console.log (friendsLength);
